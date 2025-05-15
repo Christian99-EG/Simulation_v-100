0dd27d1966887d40adb87e454964e64a0fd8b74d
Christian <72570049+Christian99-EG@users.noreply.github.com>
2025-05-15 01:16:08 -0600

    Update latticewidget.h
    modified may 15 2025

--- latticewidget.h
+++ latticewidget.h
@@ -9,24 +9,30 @@
 #define RGB_MIN 1
 #define RGB_MAX 255
 
+enum ModoDibujo{Lattice, Automata};
+
 class LatticeWidget: public QOpenGLWidget, public QOpenGLFunctions
 {
+    Q_OBJECT
+
 public:
     LatticeWidget(QWidget *parent = nullptr);
     ~LatticeWidget();
 
-protected:
-    void initializeGL() override;
-    void paintGL() override;
-    void resizeGL(int w, int h) override;
+    virtual void CrearLattice(); //metodo que construye la lattice
+    virtual void initializeGL();
+    virtual void paintGL(); //este metodo dibuja la lattice
+    //virtual void paintGL2(); //este metodo activa al automata Nota: solo se utilizara paintGL()
+    virtual void resizeGL(int w, int h);
 
-private slots:
-    void Evolucionar();
-
+public slots:
+    virtual void Evolucionar(); //metodo que comenzara con la simulacion del automata
 private:
-    void qColorToRGB(const QColor &C, float &r, float &g, float &b) const;
-    float normaliza_0_1(float val, float min, float max) const;
+   virtual void qColorToRGB(const QColor &C, float &r, float &g, float &b) const;
+   virtual float normaliza_0_1(float val, float min, float max) const;
+   bool MatrizGenerada = false;
 
+   public:
     //para la lattice
     int **matriz;
     int **ciclos;
@@ -34,9 +40,22 @@ private:
     const int cellSize = 10;
     int CicloActual = 0;
     const int maxCiclos = 100;
+    enum ModoDibujo{Lattice, Automata};
+    ModoDibujo modo = Lattice; //definimos que LatticeNormal es solo la representacion de la red
     QTimer *timer;
 
-    int ContarVecinos(int i, int j);
-};
+    //para la propagacion
+    int tiempoInfeccion, infectadosIniciales;
 
+    void setTiempoInfeccion(int t); //donde el parametro t = tiempo (entrada del tiempo)
+    void setHostInfectados(int h); //donde el parametro h = host (numero de host infectados)
+
+    virtual int ContarVecinos(int i, int j);
+    virtual void setModoDibujo(ModoDibujo NuevoModo); //cambia el color y actualiza la lattice sobre wl widget
+    int** getMatriz() const {return matriz;}
+    //bool SimulationRunning = false; //para comenzar la simulacion del automata se ocupa un booleano
+    //void setSimulationRunning(bool running);
+    void ReiniciarLattice();
+};
 #endif // LATTICEWIDGET_H
+
